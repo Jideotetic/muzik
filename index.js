86da090068d93f3c60d1720ba79e146fa7ca8a71
Jideotetic <jideotetic@gmail.com>
2023-10-21 10:23:19 +0100

    Implemented the header
    Mobile responsiveness for nav
    Added functionality for theme changing

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-const header = document.querySelector("header");
-const h1 = document.querySelector("h1");
-const hamburger = document.getElementById("mobile-menu-icon");
-const hamburgerContainer = document.querySelector("label");
-const form = document.querySelector("form");
-const searchInput = document.getElementById("search");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector("label");
+  const nav = document.querySelector("nav");
+  const preferOSThemeMode = window.matchMedia("(prefers-color-scheme: dark)");
+  const themeToggler = document.getElementById("toggle");
+  console.log("index");
 
-const tracks = [];
-
-document.addEventListener("DOMContentLoaded", fetchMusic);
-
-function fetchMusic() {}
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (header.classList.contains("fixed")) {
-    //
+  // load correct theme on initial visit or repeated visit
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    themeToggler.checked = true;
+    document.documentElement.classList.add("dark");
   } else {
-    header.classList.remove("h-screen", "relative");
-    header.classList.add("h-20", "fixed", "inset-x-0");
-    h1.classList.remove("top-1/4", "-translate-x-1/2", "left-1/2");
-    h1.classList.add("top-3", "left-3");
-    form.classList.remove("top-1/2", "translate-x-1/2", "right-1/2");
-    form.classList.add("top-3", "right-3", "opacity-0", "md:opacity-100");
-    hamburgerContainer.classList.remove("hidden");
-    hamburgerContainer.classList.add("opacity-0");
-
-    setTimeout(() => {
-      header.classList.remove("h-20");
-      header.classList.add("h-fit");
-      h1.classList.remove("absolute", "top-3", "left-3");
-      form.classList.remove("absolute", "top-3", "right-3");
-      hamburgerContainer.classList.remove("opacity-0");
-      form.classList.add(
-        "fixed",
-        "top-[60px]",
-        "inset-x-0",
-        "h-[calc(100vh-62px)]",
-        "w-0",
-        "opacity-100",
-        "md:static",
-        "md:h-fit",
-        "md:w-fit"
-      );
-    }, 1000);
+    themeToggler.checked = false;
+    document.documentElement.classList.remove("dark");
   }
 
-  searchInput.value = "";
-});
+  // change theme on change of OS theme
+  preferOSThemeMode.addEventListener("change", (e) => {
+    theme = e.matches ? "dark" : "light";
+    if (theme === "dark") {
+      themeToggler.checked = true;
+      document.documentElement.classList.add("dark");
+    } else {
+      themeToggler.checked = false;
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  });
 
-hamburger.addEventListener("change", () => {
-  if (form.classList.contains("w-0")) {
-    form.classList.remove("w-0", "opacity-0");
-    form.classList.add("w-full");
-  } else if (form.classList.contains("w-full")) {
-    form.classList.add("w-0");
-    form.classList.remove("w-full");
-  }
+  // change theme on click of themeToggler
+  themeToggler.addEventListener("change", () => {
+    if (themeToggler.checked) {
+      localStorage.theme = "dark";
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.theme = "light";
+      document.documentElement.classList.remove("dark");
+    }
+  });
+
+  hamburger.addEventListener("change", (e) => {
+    if (nav.classList.contains("w-0")) {
+      nav.classList.remove("w-0");
+      nav.classList.add("w-full");
+    } else if (nav.classList.contains("w-full")) {
+      nav.classList.remove("w-full");
+      nav.classList.add("w-0");
+    }
+  });
 });
